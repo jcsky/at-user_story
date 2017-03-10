fa9324a46c16bd5d764fd836e73528cae4564615
王信凱 <jcskyting@gmail.com>
2017-03-10 17:16:17 +0800

    sky review

--- dashboard.rb
+++ dashboard.rb
@@ -1,36 +1,38 @@
 feature '<教師 Dashboard>頁面' do
-  scenario '教學記錄欄位顯示累積上課時數及累積上課收入'do
+  scenario '教學記錄欄位顯示累積上課時數及累積上課收入' do
   end
   scenario '課程狀態欄位顯示即將上課堂數/已完成上課堂數/學生已購課堂數' do
   end
 
   feature '<教師 Dashboard_我的課表>頁面' do
-    scenario '點選行事曆上已預約但未上課之課程將會顯示課程詳情' do
+    scenario '顯示老師該月份的課程安排，月份可調整' do
     end
-    scenario '行事曆上以灰字表示過去已完成課程' do
-    end
-    scenario '行事曆上以紅字表示當天課程或未確認完成的課程' do
-    end
-    scenario '行事曆上以綠字表示已預約但未完成的課程' do
+    scenario '點選行事曆中的課程，會進到該課程詳細頁面' do
     end
   end
   feature '<教師 Dashboard_我的學生>頁面' do
-    feature '課程開始前' do
-      scenario '可按[提醒學生]來提醒已買課但未預約的學生' do
-      end
-      scenario '點選學生預約的課程後就會出現[即將上課]及[取消課堂]按鈕' do
-      end
-      scenario '一個月僅有兩次可以取消課程，之後上課會扣當課堂費50%' do
-      end
-      scenario '上課10分鐘前[即將上課]將會變成[進入教室]且[取消課程]會消失' do
+    feature '可以看到所有學生及學生資訊(名字、國家、學習語言)'
+    feature '可以看到有該老師正式課、體驗課的學生列表，正式課排序在前面' do
+      scenario '點擊顯示該學生資訊、課程包列表、傳送訊息按鈕' do
       end
     end
-    feature '課程結束後' do
-      scenario '需按[確認課堂完成]且給予學生評價來完成課程' do
-      end
-      scenario '可按[回報課程問題]來告知AT' do
-      end
-    end
+
+    # feature '課程開始前' do
+    #   scenario '可按[提醒學生]來提醒已買課但未預約的學生' do
+    #   end
+    #   scenario '點選學生預約的課程後就會出現[即將上課]及[取消課堂]按鈕' do
+    #   end
+    #   scenario '一個月僅有兩次可以取消課程，之後上課會扣當課堂費50%' do
+    #   end
+    #   scenario '上課10分鐘前[即將上課]將會變成[進入教室]且[取消課程]會消失' do
+    #   end
+    # end
+    # feature '課程結束後' do
+    #   scenario '需按[確認課堂完成]且給予學生評價來完成課程' do
+    #   end
+    #   scenario '可按[回報課程問題]來告知AT' do
+    #   end
+    # end
   end
 
   feature '<教師 Dashboard_收入明細>頁面' do
@@ -40,15 +42,17 @@ feature '<教師 Dashboard>頁面' do
     end
   end
 
-  feature '<教師 Dashboard_開課設定>頁面'do
+  feature '<教師 Dashboard_開課設定>頁面' do
     scenario '可選擇開課狀態/需備課時間/是否需要確認上課預約' do
     end
-    feature '是否需要確認上課預約' do
+    feature '老師可選擇 上課預約是否需要確認' do
       scenario '系統預設值為不需要' do
       end
-      scenario '教師須在學生預約後24小時內至<教師 Dashboard_我的學生>選擇' do
+      # scenario '教師須在學生預約後24小時內至<教師 Dashboard_我的學生>選擇' do
+      # end
+      scenario '需要: 若教師沒有在24小時內做選擇，此課將自動被取消' do
       end
-      scenario '若教師沒有在24小時內做選擇，此課將自動被取消' do
+      scenario '不需要: ...' do
       end
     end
     scenario '可看到每週固定課表及行事曆' do
@@ -60,7 +64,7 @@ feature '<教師 Dashboard>頁面' do
   feature '<教師 Dashboard_授課價格>頁面' do
     scenario '可更改正式課及體驗課的價格' do
     end
-    scenario '可新增或刪除課程包'do
+    scenario '可新增或刪除課程包' do
     end
   end
 
@@ -82,7 +86,7 @@ feature '<教師 Dashboard>頁面' do
     feature '<教師 Dashboard_教師設定_收帳設定>' do
       scenario '可改變收帳方式' do
       end
-      scenario '可更新PayPal及銀行的資料'do
+      scenario '可更新PayPal及銀行的資料' do
       end
     end
   end
@@ -145,7 +149,7 @@ feature '<學生 Dashboard>頁面'
 
   feature '<學生 Dashboard_AT幣>頁面' do
   end
-  feature '<學生 Dashboard_活動優惠券>頁面'do
+  feature '<學生 Dashboard_活動優惠券>頁面' do
   end
   feature '<學生 Dashboard_帳號設定>頁面' do
     scenario '可更改個人資料及密碼' do
