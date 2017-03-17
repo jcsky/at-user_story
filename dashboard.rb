94589e66604c5a11dcc80bd5792c210ccbe00ca6
alexhsu19960325 <alex0325khun@gmail.com>
2017-03-17 15:50:45 +0800

    教師 Dashboard 更改

--- dashboard.rb
+++ dashboard.rb
@@ -1,110 +1,138 @@
-feature '<教師 Dashboard>頁面' do
+feature '<教師 Dashboard>' do
   feature 'sidebar' do
-    scenario 'show 教學記錄(累積上課時數及累積上課收入)'do
+    scenario '顯示教學記錄(累積上課時數及累積上課收入)'do
     end
-    scenario '課程狀態欄位顯示即將上課堂數/已完成上課堂數/學生已購課堂數' do
+    scenario '顯示課程狀態(即將上課堂數/已完成上課堂數/學生已購課堂數)' do
     end
   end
 
-  feature '<教師 Dashboard_我的課表>頁面' do
+  feature '<我的課表>' do
     scenario '顯示老師該月份的課程安排，月份可調整' do
+    end
+    scenario '以顏色區分不同狀態(過去已完成、當天或未確認課、已預約但未完成的課程)' do
     end
     scenario '點選行事曆中的課程，會進到該課程詳細頁面' do
     end
   end
-  feature '<教師 Dashboard_我的學生>頁面' do
-    feature '可以看到所有學生及學生資訊(名字、國家、學習語言)'
-    feature '可以看到有該老師正式課、體驗課的學生列表，正式課排序在前面' do
-      scenario '點擊顯示該學生資訊、課程包列表、傳送訊息按鈕' do
+
+  feature '<我的學生>' do
+    feature '顯示所有學生及學生資訊(名字、國家、學習語言)' do
+      feature '可以看到有該老師正式課、體驗課的學生列表，正式課排序在前面' do
+        scenario '點擊顯示該學生資訊、課程包列表、傳送訊息按鈕' do
+        end
       end
     end
-
-    # feature '課程開始前' do
-    #   scenario '可按[提醒學生]來提醒已買課但未預約的學生' do
-    #   end
-    #   scenario '點選學生預約的課程後就會出現[即將上課]及[取消課堂]按鈕' do
-    #   end
-    #   scenario '一個月僅有兩次可以取消課程，之後上課會扣當課堂費50%' do
-    #   end
-    #   scenario '上課10分鐘前[即將上課]將會變成[進入教室]且[取消課程]會消失' do
-    #   end
-    # end
-    # feature '課程結束後' do
-    #   scenario '需按[確認課堂完成]且給予學生評價來完成課程' do
-    #   end
-    #   scenario '可按[回報課程問題]來告知AT' do
-    #   end
-    # end
-  end
-
-  feature '<教師 Dashboard_收入明細>頁面' do
-    scenario '可看到已上課紀錄及學生購課紀錄' do
+    feature '課程開始前' do
+      scenario '可按[提醒學生]來提醒已買課但未預約的學生' do
+      end
+      feature'取消課程'
+        scenario '點選學生預約的課程後就會出現[即將上課]及[取消課堂]按鈕' do
+        end
+        scenario '一個月僅有兩次可以取消課程，之後上課會扣當課堂費50%' do
+        end
+        scenario '上課10分鐘前[即將上課]將會變成[進入教室]且[取消課程]會消失' do
+        end
+      end
     end
-    scenario '可選擇想看學生購課紀錄的時間區段' do
+    feature '課程結束後' do
+      scenario '需按[確認課堂完成]且給予學生評價來完成課程' do
+      end
+      scenario '可按[回報課程問題]來知會AT’ do
+      end
     end
   end
 
-  feature '<教師 Dashboard_開課設定>頁面' do
-    scenario '可選擇開課狀態/需備課時間/是否需要確認上課預約' do
+
+  feature '<收入明細>' do
+    scenario '可選上不同年月份查看已上課紀錄及學生購課紀錄' do
     end
-    feature '老師可選擇 上課預約是否需要確認' do
-      scenario '系統預設值為不需要' do
+    scenario '可於<學生購課紀錄>看到所有這個月完成的課程數及種類/金流及手續費用/總賺取金額' do
+    end
+  end
+
+  feature '<開課設定>' do
+    feature '選擇開課狀態' do
+      scenario '開放:學生可預約課程' do
       end
-      # scenario '教師須在學生預約後24小時內至<教師 Dashboard_我的學生>選擇' do
-      # end
-      scenario '需要: 若教師沒有在24小時內做選擇，此課將自動被取消' do
-      end
-      scenario '不需要: ...' do
+      scenario '關閉:學生不可再預約課程，但須上完已被預約的課程' do
       end
     end
-    scenario '可看到每週固定課表及行事曆' do
+    feature '選擇需備課時間' do
+      scenario '設定開課前幾小時可被預約(1/6/12/24)' do
+      end
+    end
+    feature '選擇是否需要確認上課預約' do
+      scenario '需要:若教師沒有在24小時內做選擇，此課將自動被取消' do
+      end
+      scenario '不需要(系統預設):學生預約即成功' do
+      end
+    end
+    scenario '可看到每週固定課表及每個月的行事曆' do
     end
     scenario '可直接在行事曆新增或刪除時段' do
     end
   end
 
-  feature '<教師 Dashboard_授課價格>頁面' do
-    scenario '可更改正式課及體驗課的價格' do
-    end
-    scenario '可新增或刪除課程包' do
+  feature '<授課價格>' do
+    feature '設定課程價格'
+      feature '體驗課(30分鐘)' do
+        scenario '可設定為免費' do
+        end
+      end
+      feature '正式課(60分鐘)' do
+        scenario '可新增或刪除五的倍數的課程包' do
+        end
+        scenario '一/五/十的課程(包)不能刪除' do
+        end
+      end
     end
   end
 
-  feature '<教師 Dashboard_教師設定>頁面' do
-    feature '<教師 Dashboard_教師設定_教師資料>' do
+  feature '<教師設定>' do
+    feature '教師資料' do
       scenario '可新增或刪除教授語言及會說語言' do
       end
       scenario '可更改簡短介紹或詳細介紹' do
       end
     end
-    feature '<教師 Dashboard_教師設定_教師經驗>' do
-      scenario '可新增或刪除教學證照/學歷背景/工作經驗' do
+    feature '教學經驗' do
+      scenario '可新增或刪除教學證照(證照名稱及檔案)' do
+      end
+      scenario '可新增或刪除學歷背景(日期/學校及科系名稱/檔案)' do
+      end
+      scenario '可新增或刪除工作經驗(日期/公司名稱/工作名稱)' do
       end
     end
-    feature '<教師 Dashboard_教師設定_介紹影片>' do
+    feature '介紹影片' do
       scenario '可更新影片連結' do
       end
     end
-    feature '<教師 Dashboard_教師設定_收帳設定>' do
+    feature '收帳設定' do
       scenario '可改變收帳方式' do
       end
       feature '可更新PayPal及銀行的資料' do
-        scenario 'paypal需要: papal account email' do
+        scenario 'paypal需要: PayPal account email' do
         end
-        scenario '銀行需要: 1. 2. 3. ...' do
+        scenario '銀行需要: 銀行名稱/分行名稱/帳戶名/銀行代碼/帳戶號碼' do
         end
       end
     end
   end
-end
+
+
+
+
+
 
 feature '<學生 Dashboard>頁面'
-  scenario '學習紀錄欄位顯示上課總時數/出席率/AT幣' do
-  end
-  scenario '上課狀態欄位顯示預約數/未預約/已完成' do
+  feature 'sidebar' do
+    scenario '顯示學習記錄(上課總時數/出席率/AT幣)'do
+    end
+    scenario '顯示上課狀態(預約數/未預約/已完成)' do
+    end
   end
 
-  feature '<學生 Dashboard_我的課表>頁面' do
+  feature '<我的課表>' do
     scenario '行事曆上以灰字表示過去已完成課程' do
     end
     scenario '行事曆上以紅字表示當天課程但未確認狀態' do
@@ -128,7 +156,7 @@ feature '<學生 Dashboard>頁面'
       end
     end
   end
-  feature '<學生 Dashboard_預約課程>頁面' do
+  feature '<預約課程>' do
     scenario '已購課的user可在此頁面預約課程' do
     end
     scenario '行事曆上以灰字表示教師沒開課之時間' do
@@ -138,10 +166,14 @@ feature '<學生 Dashboard>頁面'
     scenario '行事曆上以紅字表示當天' do
     end
   end
-  feature '<學生 Dashboard_我的教師>頁面' do
-    scenario '未購課的user可看自己收藏的教師' do
+  feature '<我的教師>' do
+    feature '可查看已上過/已預約的正式課(或體驗課)教師及已收藏教師' do
+      scenario '點選[尋找教師]跳轉到<教師牆>便可收藏教師' do
+      end
+      scenario '點選教師即可查看課程數(已完成/已預約/未預約)' do
+      end
     end
-    feature '[取消預約]'
+    #feature '[取消預約]'
       scenario '已預約課的user可於開課12小時前[取消預約]但一個月上限兩次' do
       end
       scenario '第三次無法再取消，系統仍會開課' do
@@ -153,11 +185,11 @@ feature '<學生 Dashboard>頁面'
     end
   end
 
-  feature '<學生 Dashboard_AT幣>頁面' do
+  feature '<AT幣>' do
   end
-  feature '<學生 Dashboard_活動優惠券>頁面' do
+  feature '<活動優惠券>' do
   end
-  feature '<學生 Dashboard_帳號設定>頁面' do
+  feature '<帳號設定>' do
     scenario '可更改個人資料及密碼' do
     end
     scenario '可更改信箱及手機號碼但須重新認證' do
