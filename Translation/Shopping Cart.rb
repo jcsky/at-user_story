8412db5cbb27ce0bca37f5d407c76b69aa2e83c3
alexhsu19960325 <alex0325khun@gmail.com>
2017-03-29 01:59:12 +0800

    購物車

---
+++ Translation/Shopping Cart.rb
@@ -0,0 +1,33 @@
+Shopping_Cart:
+  title: '首頁/購物車'
+  subtitle: '購物車'
+  package_blank:
+    photo: '照片'
+    name: 'Abner Chao'
+    star: '評價星星'
+    calender: '教師行事曆'
+    can_teach: '教'
+    come_from: '來自'
+    can_speak: '會說'
+    learn: '學'
+    class_number: '堂數'
+    type: '主題'
+  price_blank:
+    name: 'Abner Chao'
+    service_charge: '服務費'
+    discount: '優惠折扣'
+    total: '總價'
+    notice: '您將使用台幣付款，價格會根據匯率的變化而有浮動'
+    choose_coupon: '選擇優惠券'
+pay:
+  title: '首頁/購物車/結帳'
+  subtitle: '結帳'
+  how_to_pay:
+    credit_card:
+      title:'選擇信用卡付款'
+    convenience_store:
+      title: '適用台灣超商繳款'
+    ATM:
+      title: '適用台灣ATM轉帳'
+  rule: '我同意服務條款與隱私聲明並在同意在付費後的退費行為僅能退至AmazingTalker作為消費幣使用，不能退回現金。我也同意支付所顯示的總金額（含服務費）'
+  agree_and_pay_btn: '確認並支付'
