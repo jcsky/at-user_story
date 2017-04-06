6591117c69ee57048d99a9d3b394416e9519fc44
juidi <allenwalker626@gmail.com>
2017-04-06 18:11:26 +0800

    Update 訊息.rb

--- 訊息.rb
+++ 訊息.rb
@@ -8,6 +8,8 @@ feature '傳送訊息' do
     end
     scenario '近期聊天子頁面依時間排列已聊過天的教師' do
     end
+    scenario '近期聊天子頁面一次顯示七筆' do
+    end
     scenario '可在搜尋欄使用名字搜尋教師' do
     end
     scenario '可同時開始多個聊天視窗' do
@@ -15,7 +17,7 @@ feature '傳送訊息' do
   end
   feature 'navigation bar 訊息' do
     # FIXME
-    scenario '列出最近??筆的聊天室，依時間排序' do
+    scenario '列出最近4筆的聊天室，依時間排序' do
     end
     scenario '按[查看全部]即會顯示所有聊天室' do
     end
